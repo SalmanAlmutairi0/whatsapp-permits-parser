3d8c284cd86ac72030d2628b7dfe126054022c1f
salman almutairi <t123y2@gmail.com>
2025-09-26 18:22:42 +0300

    Change input type from date to datetime-local for improved date and time selection

--- src/app/page.js
+++ src/app/page.js
@@ -75,7 +75,7 @@ export default function Home() {
           <label className="flex flex-col gap-1">
             <span className="font-medium text-gray-700">Start Date </span>
             <input
-              type="date"
+              type="datetime-local"
               name="startDate"
               required
               className="border border-gray-300 rounded-lg px-3 py-2 focus:outline-none focus:ring-2 focus:ring-blue-500"
